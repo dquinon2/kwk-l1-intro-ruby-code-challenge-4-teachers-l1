910611ec0f515596f79f69b7e59eebf44a42ef1c
Dayarelis Quinones <dayarelis.quinones.17@tfacorps.org>
2018-06-06 17:07:03 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,11 +1,39 @@
 #write out your code here
 
-def least_coins(cents)
+# def least_coins(cents)
+# 
+# coins{
+#   :quarters => 25, :dimes => 10, :nickels => 5, :pennies => 1
+# }
+# return coins
+# end
+# coins.each {|value| puts "#{key} => #{value}"}
+# least_coins(cents)
 
-coins{
-  :quarters => 25, :dimes => 10, :nickels => 5, :pennies => 1
+System.out.println("Enter number of cents (Negative value to quit):");
+int cents;
+cents = scan.nextInt();
+
+while (cents > 0 )
+{
+    if (cents >= 25)
+    {
+        System.out.println("Quarter");
+        cents -= 25;
+    }
+    else if ( cents >= 10 )
+    {
+        System.out.println("Dime");
+        cents -= 10;
+    }
+    else if (cents >= 5 )
+    {
+        System.out.println("Nickle");
+        cents -= 5 ;
+    }
+    else if (cents >= 1 )
+    {
+        System.out.println("Penny");
+        cents -= 1;
+    }
 }
-return coins
-end
-coins.each {|value| puts "#{key} => #{value}"}
-least_coins(cents)
