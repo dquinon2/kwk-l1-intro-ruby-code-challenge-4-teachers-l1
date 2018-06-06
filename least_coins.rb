69413a28587e416f174d6e43fba2d8c83ce85aeb
Dayarelis Quinones <dayarelis.quinones.17@tfacorps.org>
2018-06-06 17:03:09 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -5,6 +5,7 @@ def least_coins(cents)
 coins{
   :quarters => 25, :dimes => 10, :nickels => 5, :pennies => 1
 }
-puts coins
+return coins
 end
+coins.each {|value| puts "#{key} => #{value}"}
 least_coins(cents)
